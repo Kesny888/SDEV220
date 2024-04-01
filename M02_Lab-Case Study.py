5a18a660e307e87884bdb5fcaabe9c72b911679a
Kesny R <99103639+Kesny888@users.noreply.github.com>
2024-04-01 19:30:26 -0400

    Update M02_Lab-Case Study.py

--- M02_Lab-Case Study.py
+++ M02_Lab-Case Study.py
@@ -5,7 +5,7 @@
 #Create a program that determines if the student qualifies for either the Dean's List or the Honor Roll. It allow the student enter lastname, firstname and GPA. If the student entered last name is 'ZZZ', the program will quit. If the student entered GPA 3.5 or greater, the student qualify for the Dean's list. And if the student entered GPA 3.2 or greater, the student qualiify for the Honor Roll.
 
 
-StudentLname = input("Enter the student's last name: ")
+StudentLname = input("Enter the student's last name or ZZZ to quit: ")
 while (StudentLname != "ZZZ"):
     StudentFname = input("Enter the student's first name: ")
     StudentGPA = float(input("Enter the student's GPA: "))
